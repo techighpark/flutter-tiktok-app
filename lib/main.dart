cd7daa5809e9d407f72b7e8998c33dcacf4d13c4
techigh <techigh.b@gmail.com>
2023-03-04 18:52:30 +0900

    constants

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class TikTokApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFFE9435A),
       ),
       home: Container(),
     );
