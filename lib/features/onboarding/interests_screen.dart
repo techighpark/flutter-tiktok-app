f4459278422ad99f764f839d013954f6bb7608ae
techigh <techigh.b@gmail.com>
2023-03-08 22:19:16 +0900

    CupertinoButton

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
@@ -49,99 +50,87 @@ class InterestsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Choose your interests"),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: Sizes.size24,
-            right: Sizes.size24,
-            bottom: Sizes.size48,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Gaps.v32,
-              const Text(
-                'Choose your Interests',
-                style: TextStyle(
-                  fontSize: Sizes.size40,
-                  fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: const Text("Choose your interests"),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: Sizes.size24,
+              right: Sizes.size24,
+              bottom: Sizes.size48,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v32,
+                const Text(
+                  'Choose your Interests',
+                  style: TextStyle(
+                    fontSize: Sizes.size40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Gaps.v20,
-              const Text(
-                'Get better video recommendations',
-                style: TextStyle(
-                  fontSize: Sizes.size20,
-                  color: Colors.grey,
+                Gaps.v20,
+                const Text(
+                  'Get better video recommendations',
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                    color: Colors.grey,
+                  ),
                 ),
-              ),
-              Gaps.v64,
-              Wrap(
-                spacing: 15,
-                runSpacing: 15,
-                children: [
-                  for (String interest in interests)
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: Sizes.size24,
-                        vertical: Sizes.size16,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(
-                          Sizes.size32,
+                Gaps.v64,
+                Wrap(
+                  spacing: 15,
+                  runSpacing: 15,
+                  children: [
+                    for (String interest in interests)
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: Sizes.size24,
+                          vertical: Sizes.size16,
                         ),
-                        border:
-                            Border.all(color: Colors.black.withOpacity(0.1)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.05),
-                            blurRadius: 5,
-                            spreadRadius: 5,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(
+                            Sizes.size32,
                           ),
-                        ],
-                      ),
-                      child: Text(
-                        interest,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
+                          border:
+                              Border.all(color: Colors.black.withOpacity(0.1)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.05),
+                              blurRadius: 5,
+                              spreadRadius: 5,
+                            ),
+                          ],
+                        ),
+                        child: Text(
+                          interest,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        elevation: 2,
-        padding: const EdgeInsets.only(
-          bottom: Sizes.size40,
-          top: Sizes.size16,
-          left: Sizes.size24,
-          right: Sizes.size24,
-        ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size20,
-          ),
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-          ),
-          child: const Text(
-            'Next',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: Sizes.size16,
+                  ],
+                ),
+              ],
             ),
           ),
         ),
-      ),
-    );
+        bottomNavigationBar: BottomAppBar(
+          elevation: 2,
+          padding: const EdgeInsets.only(
+            bottom: Sizes.size40,
+            top: Sizes.size16,
+            left: Sizes.size24,
+            right: Sizes.size24,
+          ),
+          child: CupertinoButton(
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+            child: const Text('Next'),
+          ),
+        ));
   }
 }
