43917790b2597a2e7a3d8ad0f087f74eeb46fd60
techigh <techigh.b@gmail.com>
2023-03-08 23:00:43 +0900

    interest_button + animated

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/widgets/interest_button.dart';
 
 const interests = [
   "Daily Life",
@@ -120,33 +121,7 @@ class _InterestsScreenState extends State<InterestsScreen> {
                   runSpacing: 15,
                   children: [
                     for (String interest in interests)
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: Sizes.size24,
-                          vertical: Sizes.size16,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(
-                            Sizes.size32,
-                          ),
-                          border:
-                              Border.all(color: Colors.black.withOpacity(0.1)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.05),
-                              blurRadius: 5,
-                              spreadRadius: 5,
-                            ),
-                          ],
-                        ),
-                        child: Text(
-                          interest,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+                      InterestButton(interest: interest),
                   ],
                 ),
               ],
