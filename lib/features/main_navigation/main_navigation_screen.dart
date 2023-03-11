6294c231b47a7f3345d01c920e9760a1cc05b290
techigh <techigh.b@gmail.com>
2023-03-11 21:00:18 +0900

    main button - Stack : clipBehavior && Positioned : left||right

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/main_navigation/stf_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
@@ -33,6 +34,12 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
             child: const StfSceen(),
           ),
           Offstage(
+            offstage: _selectedIndex != 2,
+            child: Container(
+              child: const Text('123'),
+            ),
+          ),
+          Offstage(
             offstage: _selectedIndex != 3,
             child: const StfSceen(),
           ),
@@ -49,6 +56,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           padding: const EdgeInsets.all(Sizes.size12),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               NavTab(
                 text: 'Home',
@@ -64,6 +72,60 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 selectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
               ),
+              Gaps.h24,
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    right: 24,
+                    child: Container(
+                      height: 35,
+                      width: 25,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: Sizes.size8,
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff61D4F0),
+                        borderRadius: BorderRadius.circular(Sizes.size8),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 24,
+                    child: Container(
+                      height: 35,
+                      width: 25,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: Sizes.size8,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(Sizes.size8),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 35,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        Sizes.size6,
+                      ),
+                    ),
+                    child: const Center(
+                      child: FaIcon(
+                        FontAwesomeIcons.plus,
+                        color: Colors.black,
+                        size: Sizes.size20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Gaps.h24,
               NavTab(
                 text: 'Inbox',
                 isSelected: _selectedIndex == 3,
