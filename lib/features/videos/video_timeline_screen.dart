d8198ee53b349350691f972fc45a178d961ce910
techigh <techigh.b@gmail.com>
2023-03-13 19:27:37 +0900

    PageView - onPageChanged

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -8,20 +9,46 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
   List<Color> colors = [
     Colors.black,
     Colors.red,
     Colors.pink,
     Colors.blue,
   ];
+
+  void _onPagechanged(int page) {
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      colors.addAll([
+        Colors.black,
+        Colors.red,
+        Colors.pink,
+        Colors.blue,
+      ]);
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
       // pageSnapping: false,
       scrollDirection: Axis.vertical,
-      itemCount: colors.length,
+      itemCount: _itemCount,
+      onPageChanged: _onPagechanged,
       itemBuilder: (context, index) => Container(
         color: colors[index],
+        child: Center(
+          child: Text(
+            '$index',
+            style: const TextStyle(
+              fontSize: Sizes.size24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
