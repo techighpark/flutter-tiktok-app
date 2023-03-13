ecaf5d4740b4895fb18248ed02ac242bed3f63e6
techigh <techigh.b@gmail.com>
2023-03-14 00:02:06 +0900

    Video Controller && VisibilityDetector && Positioned && IgnorePointer

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -3,36 +3,54 @@ import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
-
   @override
   State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   int _itemCount = 4;
+
   final PageController _pageController = PageController();
+
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
 
   void _onPageChanged(int page) {
     _pageController.animateToPage(
       page,
-      duration: const Duration(milliseconds: 100),
-      curve: Curves.linear,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
     );
     if (page == _itemCount - 1) {
-      _itemCount += 4;
+      _itemCount = _itemCount + 4;
+      setState(() {});
     }
-    setState(() {});
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
       controller: _pageController,
-      // pageSnapping: false,
       scrollDirection: Axis.vertical,
-      itemCount: _itemCount,
       onPageChanged: _onPageChanged,
-      itemBuilder: (context, index) => const VideoPost(),
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => VideoPost(
+        onVideoFinished: _onVideoFinished,
+        index: index,
+      ),
     );
   }
 }
--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
-  const VideoPost({super.key});
+  final Function onVideoFinished;
+  final int index;
+
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+    required this.index,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
@@ -10,12 +20,21 @@ class VideoPost extends StatefulWidget {
 
 class _VideoPostState extends State<VideoPost> {
   final VideoPlayerController _videoPlayerController =
-      VideoPlayerController.asset('assets/videos.video.mp4');
+      VideoPlayerController.asset("assets/videos/video.mp4");
+
+  void _onVideoChange() {
+    VideoPlayerValue controllerValue = _videoPlayerController.value;
+    if (controllerValue.isInitialized) {
+      if (controllerValue.duration == controllerValue.position) {
+        widget.onVideoFinished();
+      }
+    }
+  }
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
     setState(() {});
+    _videoPlayerController.addListener(_onVideoChange);
   }
 
   @override
@@ -25,17 +44,56 @@ class _VideoPostState extends State<VideoPost> {
   }
 
   @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+// how to detect current video visibility?
+  void _onVisibilityChange(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
+  void _togglePause() {
+    _videoPlayerController.value.isPlaying
+        ? _videoPlayerController.pause()
+        : _videoPlayerController.play();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: _videoPlayerController.value.isInitialized
-              ? VideoPlayer(_videoPlayerController)
-              : Container(
-                  color: Colors.black,
+    return VisibilityDetector(
+      onVisibilityChanged: _onVisibilityChange,
+      key: Key("${widget.index}"),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(
+                    color: Colors.black,
+                  ),
+          ),
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: _togglePause,
+            ),
+          ),
+          Positioned.fill(
+            child: IgnorePointer(
+              child: Center(
+                child: FaIcon(
+                  FontAwesomeIcons.play,
+                  color: Colors.white.withOpacity(0.8),
+                  size: Sizes.size52,
                 ),
-        )
-      ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
