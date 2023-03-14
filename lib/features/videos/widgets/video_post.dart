17b155b2e2377f00ae95ae3326366c0e241619f4
techigh <techigh.b@gmail.com>
2023-03-14 23:26:33 +0900

    CircleAvatar && SingleTickerProviderStateMixin && AnimationController

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_button.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
-// Video Player
-// Visibility
-// Custom Animation
-//  - SingleTickerProviderStateMixin && vsync
-//
+/**video post
+ * [VideoPlayer]
+ * [VisibilityDetector]
+ * - onVisibilityChanged
+ * Custom Animation
+ * - [AnimatiionController]
+ * - [SingleTickerProviderStateMixin]
+ *  : Provides a single Ticker that is configured to only tick while the current tree is enabled, as defined by TickerMode.
+ *  [TickerProvideerStateMixin] - multifple
+ * - vsync: this
+ *  : 위젯이 안 보일 때는 애니메이션 동작 prevent
+ *  : prevent to waste resource
+ * [AnimatedBuilder]
+ */ ///
 
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
@@ -30,6 +41,11 @@ class _VideoPostState extends State<VideoPost>
       VideoPlayerController.asset("assets/videos/video.mp4");
   late final AnimationController _animationController;
 
+  final List<String> _hashtags = ['hash', 'tag', 'tiktok', 'wtf'];
+  final String _text =
+      'This is a very long text that will be truncated if it exceeds the container.';
+  late bool _showMore;
+
   bool _isPaused = false;
   final Duration _animationDuration = const Duration(milliseconds: 200);
 
@@ -44,14 +60,16 @@ class _VideoPostState extends State<VideoPost>
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
-    setState(() {});
+    await _videoPlayerController.setLooping(true);
     _videoPlayerController.addListener(_onVideoChange);
+    setState(() {});
   }
 
-// QUESTION : why don't set state in addListener - video controller
-// QUESTION : why set state in addListener - animation controller
+// [Q] : why don't set state in addListener - video controller
+// [Q] : why set state in addListener - animation controller
   void _initAnimation() {
     _animationController = AnimationController(
+      // this = ticker
       vsync: this,
       lowerBound: 1.0,
       upperBound: 1.5,
@@ -59,25 +77,26 @@ class _VideoPostState extends State<VideoPost>
       duration: _animationDuration,
     );
     // every time setState
-    _animationController.addListener(() {
-      setState(() {});
-    });
+    // _animationController.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  void _initShowMore() {
+    _showMore = _text.length < 30;
+    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
+
     _initVideoPlayer();
     _initAnimation();
+    _initShowMore();
   }
 
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    super.dispose();
-  }
-
-// QUESTION: how to detect current video visibility?
+// [Q]: how to detect current video visibility?
   void _onVisibilityChange(VisibilityInfo info) {
     if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
       _videoPlayerController.play();
@@ -101,10 +120,23 @@ class _VideoPostState extends State<VideoPost>
     setState(() {});
   }
 
+  void _onTextTap() {
+    _showMore = !_showMore;
+    print(_showMore);
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return VisibilityDetector(
       onVisibilityChanged: _onVisibilityChange,
+      // Q
       key: Key("${widget.index}"),
       child: Stack(
         children: [
@@ -124,21 +156,110 @@ class _VideoPostState extends State<VideoPost>
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: Transform.scale(
-                  scale: _animationController.value,
-                  child: AnimatedOpacity(
-                    duration: _animationDuration,
-                    opacity: _isPaused ? 1 : 0,
-                    child: FaIcon(
-                      FontAwesomeIcons.play,
-                      color: Colors.white.withOpacity(0.8),
-                      size: Sizes.size52,
+                // 2nd method of animate
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: _animationController.value,
+                      child: child,
+                    );
+                  },
+                  child: Transform.scale(
+                    scale: _animationController.value,
+                    child: AnimatedOpacity(
+                      duration: _animationDuration,
+                      opacity: _isPaused ? 1 : 0,
+                      child: FaIcon(
+                        FontAwesomeIcons.play,
+                        color: Colors.white.withOpacity(0.8),
+                        size: Sizes.size52,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          )
+          ),
+          Positioned(
+            bottom: 30,
+            left: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '@xpzm',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: Sizes.size20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v16,
+                GestureDetector(
+                  onTap: _onTextTap,
+                  child: Text(
+                    'This is a very long text',
+                    overflow: _showMore ? null : TextOverflow.ellipsis,
+                    maxLines: 4,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size16,
+                    ),
+                  ),
+                ),
+                Gaps.v16,
+                Row(
+                  children: [
+                    for (String tag in _hashtags)
+                      Row(
+                        children: [
+                          Text(
+                            '#$tag',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: Sizes.size16,
+                            ),
+                          ),
+                          Gaps.h2,
+                        ],
+                      ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            right: 20,
+            child: Column(
+              children: const [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.deepOrange,
+                  foregroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/75081212?v=4'),
+                  child: Text('xpzm'),
+                ),
+                Gaps.v24,
+                VideoButton(
+                  icon: FontAwesomeIcons.solidHeart,
+                  text: "2.9M",
+                ),
+                Gaps.v24,
+                VideoButton(
+                  icon: FontAwesomeIcons.solidComment,
+                  text: "36K",
+                ),
+                Gaps.v24,
+                VideoButton(
+                  icon: FontAwesomeIcons.share,
+                  text: "Share",
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
