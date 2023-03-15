fe4e056d459524fd8328d1f94522b61c9570d7b1
techigh <techigh.b@gmail.com>
2023-03-15 22:52:31 +0900

    Comment - Scaffold : appBar + bottomAppBar && bottomSheet : transparent && ListView.seperated && Expand

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -36,13 +37,85 @@ class _VideoCommentsState extends State<VideoComments> {
             )
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+            vertical: Sizes.size10,
+          ),
           itemCount: 10,
+          separatorBuilder: (context, index) => Gaps.v32,
           itemBuilder: (context, index) {
-            return Container(
-              child: const Text('comment'),
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  radius: Sizes.size20,
+                  child: Text('xpzm'),
+                ),
+                Gaps.h12,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'xpzm',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Gaps.v4,
+                      Text(
+                        'techigh take high, your can do! 😀 😀asfsfsafsdfasdfasfasfasfasfsafasfasfasfsa',
+                        style: TextStyle(
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade900,
+                        ),
+                      ),
+                      Gaps.v4,
+                      Text(
+                        'techigh take high, your can do! 😀',
+                        style: TextStyle(
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.h10,
+                Column(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                      color: Colors.grey.shade500,
+                      size: Sizes.size20,
+                    ),
+                    Text(
+                      '23.4K',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    )
+                  ],
+                )
+              ],
             );
           },
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: Sizes.size20,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text('xpzm'),
+              )
+            ],
+          ),
         ),
       ),
     );
